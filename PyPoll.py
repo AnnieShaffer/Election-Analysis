19fe769676065ab3f4c21e3e5d44cf2ba3580dc7
Annie Shaffer <annie.shaffer15@gmail.com>
2020-08-01 14:37:27 -0400

    updated .py file

--- PyPoll.py
+++ PyPoll.py
@@ -5,13 +5,34 @@
 # 4. The total number of votes each canditate won
 # 5. The winner of the election based on the votes
 
+# Add dependencies
 import csv
 import os
-# Assign a variable for the file to load and the path
+
+# Assign a variable to load a file from a path
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
+# Assign a variable to save the file to a path
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-    # Print the file object
-    print(election_data)
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row
+    headers = next(file_reader)
+    print(headers)
+
+    # To do: read and analyze the data here
+    # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+
+    #print each row in the CSV file
+    for row in file_reader:
+            print(row)
+
+with open(file_to_save, "w") as txt_file:
+
+    txt_file.write("Counties in the election")
+    txt_file.write("\n-------------------------")
+    txt_file.write("\nArapahoe\nDenver\nJefferson")
